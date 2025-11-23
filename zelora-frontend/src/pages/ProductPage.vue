4e740b02794b5fbd472c7daa6ed989dbad049281
Dauud10 <k00263519@student.tus.ie>
2025-11-23 06:45:53 +0000

    Updated ProductPage.vue to save viewed products

--- zelora-frontend/src/pages/ProductPage.vue
+++ zelora-frontend/src/pages/ProductPage.vue
@@ -1,9 +1,20 @@
 <script setup>
 import { useRoute } from 'vue-router'
+import { addRecentlyViewed } from "../services/recentlyViewed.js";
 
-/*
-  Product detail page
-*/
+onMounted(async () => {
+  const id = route.params.id;
+  const res = await axios.get(`/api/products/${id}/detail`);
+  product.value = res.data;
+
+  // Add to recently viewed storage
+  addRecentlyViewed({
+    id: res.data.id,
+    name: res.data.name,
+    image: res.data.featureImage,
+    price: res.data.displayPrice,
+  });
+});
 
 const route = useRoute()
 const productId = route.params.id

8317dd294c369247e81cc4057a1ce5b041c92c2e
Dauud10 <k00263519@student.tus.ie>
2025-11-23 02:11:27 +0000

    Added basic SearchPage with a search box.

---
+++ zelora-frontend/src/pages/SearchPage.vue
@@ -0,0 +1,23 @@
+<script setup>
+/*
+  Basic SearchPage, NOT connected to backend yet.
+*/
+
+import { ref } from 'vue'
+
+const searchText = ref("")
+</script>
+
+<template>
+  <div>
+    <h1>Search Products</h1>
+
+    <input
+        v-model="searchText"
+        type="text"
+        placeholder="Search by name..."
+    />
+
+    <p>Search text: {{ searchText }}</p>
+  </div>
+</template>

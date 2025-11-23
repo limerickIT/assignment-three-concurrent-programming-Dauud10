a79c7205b861b5e203db132d96bab8b62e67abdc
Dauud10 <k00263519@student.tus.ie>
2025-11-23 02:12:37 +0000

    Created ProductPage and wired it to route params.

---
+++ zelora-frontend/src/pages/ProductPage.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useRoute } from 'vue-router'
+
+/*
+  Product detail page
+*/
+
+const route = useRoute()
+const productId = route.params.id
+</script>
+
+<template>
+  <div>
+    <h1>Product #{{ productId }}</h1>
+
+    <p>Full product details will go here.</p>
+  </div>
+</template>

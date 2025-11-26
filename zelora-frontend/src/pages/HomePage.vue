d03175447f3c0101d08bc2a0f0e260806c6804f5
Dauud10 <k00263519@student.tus.ie>
2025-11-26 23:07:52 +0000

    updated Homepage similiar to Nike/JDSports website

--- zelora-frontend/src/pages/HomePage.vue
+++ zelora-frontend/src/pages/HomePage.vue
@@ -1,18 +1,59 @@
 <script setup>
-/* Very simple temporary homepage */
+/* Homepage */
 </script>
 
 <template>
-  <div>
+  <div class="home-container">
     <h1>Zelora — Sustainable Fashion</h1>
-    <p>Welcome to Zelora. Use the search page to browse products.</p>
+    <p class="subtitle">Eco-friendly fashion built with style.</p>
 
-    <router-link to="/search">Go to Search</router-link>
+    <router-link to="/search" class="home-btn">
+      Browse Products
+    </router-link>
   </div>
 </template>
 
 <style scoped>
+.home-container {
+  text-align: center;
+  margin-top: 120px;
+
+
+  background: linear-gradient(to bottom, #f2f2f2, #d9d9d9);
+  padding: 80px 20px;
+  border-radius: 14px;
+  max-width: 700px;
+  margin-left: auto;
+  margin-right: auto;
+  box-shadow: 0 0 25px rgba(0,0,0,0.15);
+}
+
 h1 {
-  font-size: 2rem;
+  color: #111;
+  font-size: 2.5rem;
+  font-weight: 700;
+  margin-bottom: 10px;
+}
+
+.subtitle {
+  font-size: 1.1rem;
+  color: #444;
+  margin-bottom: 30px;
+}
+
+
+.home-btn {
+  background: #111;
+  color: white;
+  padding: 12px 25px;
+  border-radius: 8px;
+  text-decoration: none;
+  font-size: 1.1rem;
+  font-weight: 600;
+  transition: 0.2s;
+}
+
+.home-btn:hover {
+  background: #333;
 }
 </style>
